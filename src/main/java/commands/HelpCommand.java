f816d329501fbc059fac7bc29b8449c0aa6d39ff
DavinciDelta <110587475+DavinciDelta@users.noreply.github.com>
2023-10-13 13:57:18 +0800

    Basic abstract Command
    Included the basic abstract command for reference, with delete code implemented

---
+++ src/main/java/commands/HelpCommand.java
@@ -0,0 +1,10 @@
+package commands;
+
+import seedu.duke.Parser;
+
+public class HelpCommand extends Command {
+    @Override
+    public void execute(String statement, Parser parser) throws IllegalArgumentException {
+
+    }
+}
