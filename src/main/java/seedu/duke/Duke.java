f216e3a4f23e7755def8439c3fcc98bb21e7e965
yingx9 <lyingx99@gmail.com>
2023-10-14 16:48:48 +0800

    Update Duke with code from AddressBook

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,21 +1,131 @@
 package seedu.duke;
 
-import java.util.Scanner;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
+import seedu.duke.commands.Command;
+import seedu.duke.commands.CommandResult;
+import seedu.duke.commands.ExitCommand;
+import seedu.duke.data.AddressBook;
+import seedu.duke.data.person.ReadOnlyPerson;
+import seedu.duke.parser.Parser;
+import seedu.duke.storage.StorageFile;
+import seedu.duke.storage.StorageFile.InvalidStorageFilePathException;
+import seedu.duke.storage.StorageFile.StorageOperationException;
+import seedu.duke.ui.TextUi;
+
+
+/**
+ * Entry point of the Address Book application.
+ * Initializes the application and starts the interaction with the user.
+ */
 public class Duke {
-    /**
-     * Main entry-point for the java.duke.Duke application.
-     */
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+    /** Version info of the program. */
+    public static final String VERSION = "AddressBook Level 2 - Version 1.0";
+
+    private TextUi ui;
+    private StorageFile storage;
+    private AddressBook addressBook;
+
+    /** The list of person shown to the user most recently.  */
+    private List<? extends ReadOnlyPerson> lastShownList = Collections.emptyList();
+
+
+    public static void main(String... launchArgs) {
+        new Duke().run(launchArgs);
     }
+
+    /** Runs the program until termination.  */
+    public void run(String[] launchArgs) {
+        start(launchArgs);
+        runCommandLoopUntilExitCommand();
+        exit();
+    }
+
+    /**
+     * Sets up the required objects, loads up the data from the storage file, and prints the welcome message.
+     *
+     * @param launchArgs arguments supplied by the user at program launch
+     *
+     */
+    private void start(String[] launchArgs) {
+        try {
+            this.ui = new TextUi();
+            this.storage = initializeStorage(launchArgs);
+            this.addressBook = storage.load();
+            ui.showWelcomeMessage(VERSION, storage.getPath());
+
+        } catch (InvalidStorageFilePathException | StorageOperationException e) {
+            ui.showInitFailedMessage();
+            /*
+             * ==============NOTE TO STUDENTS=========================================================================
+             * We are throwing a RuntimeException which is an 'unchecked' exception. Unchecked exceptions do not need
+             * to be declared in the method signature.
+             * The reason we are using an unchecked exception here is because the caller cannot reasonably be expected
+             * to recover from an exception.
+             * Cf https://docs.oracle.com/javase/tutorial/essential/exceptions/runtime.html
+             * =======================================================================================================
+             */
+            throw new RuntimeException(e);
+        }
+    }
+
+    /** Prints the Goodbye message and exits. */
+    private void exit() {
+        ui.showGoodbyeMessage();
+        System.exit(0);
+    }
+
+    /** Reads the user command and executes it, until the user issues the exit command.  */
+    private void runCommandLoopUntilExitCommand() {
+        Command command;
+        do {
+            String userCommandText = ui.getUserCommand();
+            command = new Parser().parseCommand(userCommandText);
+            CommandResult result = executeCommand(command);
+            recordResult(result);
+            ui.showResultToUser(result);
+
+        } while (!ExitCommand.isExit(command));
+    }
+
+    /** Updates the {@link #lastShownList} if the result contains a list of Persons. */
+    private void recordResult(CommandResult result) {
+        final Optional<List<? extends ReadOnlyPerson>> personList = result.getRelevantPersons();
+        if (personList.isPresent()) {
+            lastShownList = personList.get();
+        }
+    }
+
+    /**
+     * Executes the command and returns the result.
+     *
+     * @param command user command
+     * @return result of the command
+     */
+    private CommandResult executeCommand(Command command) {
+        try {
+            command.setData(addressBook, lastShownList);
+            CommandResult result = command.execute();
+            storage.save(addressBook);
+            return result;
+        } catch (Exception e) {
+            ui.showToUser(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Creates the StorageFile object based on the user specified path (if any) or the default storage path.
+     * @param launchArgs arguments supplied by the user at program launch
+     * @throws InvalidStorageFilePathException if the target file path is incorrect.
+     */
+    private StorageFile initializeStorage(String[] launchArgs) throws InvalidStorageFilePathException {
+        boolean isStorageFileSpecifiedByUser = launchArgs.length > 0;
+        return isStorageFileSpecifiedByUser ? new StorageFile(launchArgs[0]) : new StorageFile();
+    }
+
+
 }
