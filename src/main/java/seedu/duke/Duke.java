ab8128b2afeb7daaed3f2e523a09458ac7bf68e4
yingx9 <lyingx99@gmail.com>
2023-10-14 16:56:06 +0800

    Edit Main code

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,5 +1,5 @@
 package seedu.duke;
-
+import java.util.Scanner;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -34,6 +34,16 @@ public class Duke {
 
 
     public static void main(String... launchArgs) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What is your name?");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello " + in.nextLine());
         new Duke().run(launchArgs);
     }
 
