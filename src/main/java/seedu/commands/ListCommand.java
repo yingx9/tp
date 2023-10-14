bffe4d35fbfddbd14885722e201bf230cd6a9453
yingx9 <lyingx99@gmail.com>
2023-10-14 21:07:08 +0800

    Update ListCommand and edit Resource and Book
    As Resource is a generic class, Book can inherit and override
    toString to print its own specific details
    
    Hence,
    Resource.java: Add toString method to be overridden by Book
    Book.java: Override parent toString method

--- src/main/java/seedu/commands/ListCommand.java
+++ src/main/java/seedu/commands/ListCommand.java
@@ -1,15 +1,23 @@
 package seedu.commands;
 
 import seedu.parser.Parser;
-
 public class ListCommand extends Command {
     @Override
     public void execute(String statement, Parser parser) throws IllegalArgumentException {
         int size = parser.taskList.size();
-        System.out.println("list:");
-        for(int temp = 0; temp < size; temp += 1){
-            System.out.print((temp+1) + ": ");
-            System.out.println(parser.taskList.get(temp).getTitle());
+
+        if (size == 0){
+            System.out.println("There are 0 resources in the library. \n");
+        } else {
+            System.out.println("Listing all Resources in the Library: \n");
+            for (int i = 0; i < size; i += 1) {
+                String resourceDetails = parser.taskList.get(i).toString();
+                System.out.println(i+1 + ". " + resourceDetails + System.lineSeparator());
+
+            }
+
+            System.out.println("There are currently " + size + " resource(s).\n");
         }
     }
+
 }
