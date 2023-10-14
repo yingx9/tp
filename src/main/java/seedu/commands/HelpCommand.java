3806b18ebb2a19a8bb7a2012027cda0f2f705459
bnjm2000 <bnjm2000@gmail.com>
2023-10-14 21:11:20 +0800

    Moved help message to UI class

--- src/main/java/seedu/commands/HelpCommand.java
+++ src/main/java/seedu/commands/HelpCommand.java
@@ -1,17 +1,13 @@
 package seedu.commands;
 
 import seedu.parser.Parser;
+import seedu.ui.UI;
 
 public class HelpCommand extends Command {
+    private static UI ui;
     @Override
     public void execute(String statement, Parser parser) throws IllegalArgumentException {
-        System.out.println("Commands available:");
-        System.out.println("add: add an item (e.g. add /n NAME /a AUTHOR /t TAG /IBSN IBSN [/g GENRE])");
-        System.out.println("delete: delete an item (e.g. delete /id 123456789)");
-        System.out.println("list: (e.g. list /tag B /genre Fiction /author J. K. Rowling)");
-        System.out.println("find: (e.g. find /ISBN 9780763630188)");
-        System.out.println("exit: (e.g. exit");
-        System.out.println("For more information, please refer to our user guide at:" +
-                "https://ay2324s1-cs2113t-w11-1.github.io/tp/");
+        ui = new UI();
+        ui.showHelpMessage();
     }
 }
--- src/main/java/seedu/ui/UI.java
+++ src/main/java/seedu/ui/UI.java
@@ -29,6 +29,18 @@ public class UI {
         showLine();
     }
 
+    public void showHelpMessage(){
+        System.out.println("Commands available:");
+        System.out.println("add: add an item (e.g. add /n NAME /a AUTHOR /t TAG /IBSN IBSN [/g GENRE])");
+        System.out.println("delete: delete an item (e.g. delete /id 123456789)");
+        System.out.println("list: (e.g. list /tag B /genre Fiction /author J. K. Rowling)");
+        System.out.println("find: (e.g. find /ISBN 9780763630188)");
+        System.out.println("exit: (e.g. exit)");
+        System.out.println("For more information, please refer to our user guide at:" +
+                "https://ay2324s1-cs2113t-w11-1.github.io/tp/UserGuide.html");
+        showLine();
+    }
+
     public void showError(Exception e){
         System.out.println(e);
     }
