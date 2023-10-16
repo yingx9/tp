b62afec7114450326fdb9237456383a9845f62dd
yingx9 <lyingx99@gmail.com>
2023-10-16 21:35:25 +0800

    Fix check style error

--- src/test/java/seedu/commands/ListCommandTest.java
+++ src/test/java/seedu/commands/ListCommandTest.java
@@ -33,9 +33,6 @@ public class ListCommandTest {
         assertNoFilteredListDisplay();
     }
     @Test
-    /**
-     * Asserts that there are no errors when list is called and there are no resources in the library
-     */
     private void assertEmptyListMessage() throws SysLibException {
 
         String outputMessage = testUtil.getOutputMessage(listCommand, "list");
@@ -46,19 +43,12 @@ public class ListCommandTest {
     }
 
     @Test
-    /**
-     * Asserts that there an exception is thrown when the user enters a /tag without a value
-     */
     private void assertListByTagBehavior() {
         parser.resourceList = testResourceList;
         assertThrows(SysLibException.class, ()->listCommand.execute("/tag", parser));
 
     }
-
     @Test
-    /**
-     * Asserts that there an exception is thrown when the user enters a /g without a value
-     */
     private void assertListByGenreBehavior()  {
         parser.resourceList = testResourceList;
         assertThrows(SysLibException.class, ()->listCommand.execute("/g", parser));
@@ -66,9 +56,6 @@ public class ListCommandTest {
     }
 
     @Test
-    /**
-     * Asserts that there is no error when there are no resources to list with given filters.
-     */
     private void assertNoFilteredListDisplay() throws SysLibException {
         String outputMessage = testUtil.getOutputMessage(listCommand, "list /g Thriller", testResourceList);
         String expectedMessage = "Listing all resources matching given genre: " + System.lineSeparator()
