18ae4568148b6a244a6954b2d645a6396383d92e
DavinciDelta <110587475+DavinciDelta@users.noreply.github.com>
2023-10-16 11:54:57 +0800

    Add Delete Command
    Added the delete command with it's parser

--- src/main/java/seedu/commands/DeleteCommand.java
+++ src/main/java/seedu/commands/DeleteCommand.java
@@ -1,14 +1,32 @@
 package seedu.commands;
 
+import seedu.data.Book;
+import seedu.data.Resource;
+import seedu.data.SysLibException;
+import seedu.parser.DeleteParser;
 import seedu.parser.Parser;
+
+import java.util.ArrayList;
 
 public class DeleteCommand extends Command {
     @Override
-    public void execute(String statement, Parser parser) throws IllegalArgumentException {
-        int number = parseInt(statement, parser);
-        System.out.println("This resource is removed: ");
-        System.out.println(parser.resourceList.get(number));
-        parser.resourceList.remove(number);
+    public void execute(String statement, Parser parser) throws SysLibException {
+        int id = parseInt(DeleteParser.parseDelete(statement));
+        ArrayList<Resource> toRemove = new ArrayList<>();
+        System.out.println("Looking for ID: " + id + "...");
+        for (Resource r: parser.resourceList){
+            Book b = (Book) r;
+            if (b.getId() == id){
+                System.out.println("This resource is removed: ");
+                System.out.println(b);
+                toRemove.add(r);
+            }
+        }
+        if(toRemove.isEmpty()){
+            System.out.println("No resources with id matching " + id);
+        } else {
+            parser.resourceList.removeAll(toRemove);
+        }
     }
 
 }
