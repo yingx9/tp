97bad5aea57f5f451ee8ca636e72807e15defeb3
bnjm2000 <bnjm2000@gmail.com>
2023-10-17 14:39:24 +0800

    Fix helpCommand to be consistent with UG

--- src/main/java/seedu/ui/UI.java
+++ src/main/java/seedu/ui/UI.java
@@ -30,11 +30,14 @@ public class UI {
 
     public void showHelpMessage(){
         System.out.println("Commands available:");
-        System.out.println("add: add an item (e.g. add /id ID /t TITLE /a AUTHOR /tag TAG /i ISBN [/g GENRE])");
-        System.out.println("delete: delete an item (e.g. delete /id 123456789)");
-        System.out.println("list: (e.g. list /tag B /g Fiction /a J. K. Rowling /i 9780763630188)");
-        System.out.println("find: (e.g. find /i 9780763630188)");
-        System.out.println("exit: (e.g. exit)");
+        System.out.println("add: adds a new resource to the library inventory." +
+                "(e.g. add /id ID /t TITLE /a AUTHOR /tag TAG /i ISBN [/g GENRE])");
+        System.out.println("delete: deletes the resource with the specified ID from the library inventory. " +
+                "(e.g. delete /id 123456789)");
+        System.out.println("list: list all resources OR filter by certain tags or genre." +
+                "(e.g. list /tag B /g Fiction /a J. K. Rowling /i 9780763630188)");
+        System.out.println("find: find a resource by title, author, ISBN or given id. (e.g. find /i 9780763630188)");
+        System.out.println("exit: displays a farewell message and exits the program (e.g. exit)");
         System.out.println("For more information, please refer to our user guide at:" +
                 "https://ay2324s1-cs2113t-w11-1.github.io/tp/UserGuide.html");
         showLine();
