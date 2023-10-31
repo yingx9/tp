135648c5a1b3eb4c149a07312cad94b2f064c45f
DavinciDelta <110587475+DavinciDelta@users.noreply.github.com>
2023-10-31 22:58:02 +0800

    Add testing for event commands
    Add testings for eventAdd, eventDelete, eventList

---
+++ src/test/java/seedu/commands/events/EventAddCommandTest.java
@@ -0,0 +1,48 @@
+package seedu.commands.events;
+
+import org.junit.jupiter.api.Test;
+import seedu.data.SysLibException;
+import seedu.parser.Parser;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class EventAddCommandTest {
+    private final Parser parser = new Parser();
+    private final EventAddCommand eventAddCommand = new EventAddCommand();
+
+    @Test
+    public void eventAddCommandValidData() throws SysLibException {
+        eventAddCommand.execute("/t testrun /date 1-12-2001 /desc testing 123", parser);
+        String output = parser.eventList.get(0).toString();
+        String expectedOutput = "testrun | 01-12-2001 | testing 123";
+        assertEquals(output, expectedOutput);
+    }
+
+    @Test
+    public void eventAddCommandOutput() throws SysLibException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        eventAddCommand.execute("/t testrun /date 1-12-2001 /desc testing 123", parser);
+        String output = outputStream.toString();
+        String expectedOutput = "Event inserted at: 0" + System.lineSeparator() +
+                "____________________________________________________________" + System.lineSeparator();
+
+        assertEquals(expectedOutput, output);
+    }
+
+    @Test
+    public void eventAddCommandInvalidId() {
+        String input = "/t testrun /date 1 May 2001 /desc testing 123";
+        assertThrows(IllegalArgumentException.class, ()->eventAddCommand.execute(
+                "/t testrun /date 1 May 2001 /desc testing 123", parser));
+    }
+
+    @Test
+    public void eventAddCommandInsufficientData() {
+        assertThrows(IllegalArgumentException.class, ()->eventAddCommand.execute("/t ", parser));
+    }
+}
