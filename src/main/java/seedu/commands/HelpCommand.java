b8f584c61da81bd0893d555f16bab5c300f15457
bnjm2000 <bnjm2000@gmail.com>
2023-10-31 23:33:55 +0800

    Fix logs for help and exit command

--- src/main/java/seedu/commands/HelpCommand.java
+++ src/main/java/seedu/commands/HelpCommand.java
@@ -1,14 +1,19 @@
 package seedu.commands;
 
 import java.io.IOException;
+
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 import seedu.parser.Parser;
+
 import seedu.ui.UI;
 
+/**
+ * Command to print help message
+ */
 public class HelpCommand extends Command {
     private static final Logger logger = Logger.getLogger(HelpCommand.class.getName());
 
@@ -17,28 +22,39 @@ public class HelpCommand extends Command {
     static {
         // remove logs from showing in stdout
         try {
+
             Logger rootLogger = Logger.getLogger("");
             for (java.util.logging.Handler handler : rootLogger.getHandlers()) {
+
                 if (handler instanceof java.util.logging.ConsoleHandler) {
+
                     rootLogger.removeHandler(handler);
+
                 }
+
             }
 
-            FileHandler fileHandler = new FileHandler("logs/findCommandLogs.log", true);
+            FileHandler fileHandler = new FileHandler("logs/helpCommandLogs.log", true);
             fileHandler.setFormatter(new SimpleFormatter());
             LOGGER.addHandler(fileHandler);
             LOGGER.setLevel(Level.INFO);
+
         } catch (IOException e) {
+
             LOGGER.log(Level.SEVERE, "Failed to set up log file handler", e);
+
         }
     }
 
     @Override
     public void execute(String statement, Parser parser) throws IllegalArgumentException {
+
         assert statement != null : "Statement to execute cannot be null";
         assert parser != null : "Parser must not be null";
         UI ui = new UI();
         logger.log(Level.INFO, "Executing HelpCommand");
         ui.showHelpMessage();
+
     }
+
 }
