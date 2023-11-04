58ffbd1dea1b888633df6038147629e1061be767
DavinciDelta <110587475+DavinciDelta@users.noreply.github.com>
2023-11-04 23:17:13 +0800

    Remove redundant imports

--- src/main/java/seedu/data/Event.java
+++ src/main/java/seedu/data/Event.java
@@ -1,9 +1,7 @@
 package seedu.data;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 public class Event {
     private final String name;
