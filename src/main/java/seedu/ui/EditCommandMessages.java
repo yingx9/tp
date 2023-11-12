77b5dac9c4ed806a54116edf77c1f9401671a4b2
yingx9 <lyingx99@gmail.com>
2023-11-12 15:41:06 +0800

    Fix wrong argument message error for CD

--- src/main/java/seedu/ui/EditCommandMessages.java
+++ src/main/java/seedu/ui/EditCommandMessages.java
@@ -11,8 +11,8 @@ public class EditCommandMessages {
                     "ENewspapers: /t TITLE /p PUBLISHER /ed EDITION /s STATUS " + "/l LINK /i ISBN");
     public static final String BOOK_ARGS_MESSAGE = "For Books: /t TITLE /a AUTHOR /g GENRES /s STATUS /i ISBN" +
             formatLastLineDivider("For EBooks: /t TITLE /a AUTHOR /g GENRES /s STATUS /l LINK /i ISBN");
-    public static final String CD_ARGS_MESSAGE = formatLastLineDivider("For CDs: /t TITLE /p PUBLISHER" +
-            " /ed EDITION /s STATUS /i ISBN");
+    public static final String CD_ARGS_MESSAGE = formatLastLineDivider("For CDs: /t TITLE /c CREATOR" +
+            " /ty TYPE /s STATUS /i ISBN");
     public static final String MAGAZINE_ARGS_MESSAGE = "For Magazines: /t TITLE /b BRAND /is ISSUE /s STATUS /i ISBN" +
             formatLastLineDivider("For EMagazines: /t TITLE /b BRAND /is ISSUE /s STATUS /l LINK /i ISBN");
 
