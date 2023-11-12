2e08a1f5936ff70256cb180cd0bc23088f16b2d6
bnjm2000 <bnjm2000@gmail.com>
2023-11-13 01:46:54 +0800

    Beautify help message and shorten URL to UG

--- src/main/java/seedu/ui/UI.java
+++ src/main/java/seedu/ui/UI.java
@@ -65,26 +65,29 @@ public class UI {
 
     public void showHelpMessage() {
         System.out.println("Commands available:");
-        System.out.println("add: adds a new resource to the library inventory." +
+        System.out.println("[add] adds a new resource to the library inventory. " +
                 "(e.g. add /i ISBN /t TITLE /a AUTHOR /tag TAG [/g GENRE /s STATUS])");
-        System.out.println("delete: deletes the resource with the specified ID from the library inventory. " +
+        System.out.println("[delete] deletes the resource with the specified ID from the library inventory. " +
                 "(e.g. delete /id 123456789)");
-        System.out.println("list: list all resources OR filter by certain tags, genre, or status. " +
+        System.out.println("[list] lists all resources OR filter by certain tags, genre, or status. " +
                 "(e.g. list /tag B /g Fiction /s AVAILABLE)");
-        System.out.println("find: find a resource by title, author, ISBN or given id. (e.g. find /i" +
-                " 9780763630188 /a AUTHOR)");
-        System.out.println("edit: Edit a listing by entering its id to update its details. " +
+        System.out.println("[find] finds a resource by title, author, ISBN or given id. " +
+                "(e.g. find /i 9780763630188 /a AUTHOR)");
+        System.out.println("[edit] edits a listing by entering its id to update its details. " +
                 "(e.g. edit /id 123 /t NEW_TITLE /a NEW_AUTHOR)");
-        System.out.println("eventadd: Add an event to the event list " +
+        System.out.println("[eventadd] adds an event to the database. " +
                 "(e.g. eventadd /t TITLE /date 23 Dec 2023 [/desc DESCRIPTION])");
-        System.out.println("eventlist: List out all the event list (e.g. eventlist)");
-        System.out.println("eventdelete: Delete an event in the event list based on the index " +
+        System.out.println("[eventlist] lists out all events in the database. " +
+                "(e.g. eventlist)");
+        System.out.println("[eventdelete] deletes an event from the database based on the index. " +
                 "(e.g. eventdelete /i INDEX)");
-        System.out.println("eventedit: Edit an event in the event list based on the index " +
+        System.out.println("[eventedit] edits an event in the event list based on the information given. " +
                 "(e.g. eventedit /i INDEX [/t TITLE /date DATE /desc DESCRIPTION])");
-        System.out.println("exit: displays a farewell message and exits the program (e.g. exit)");
-        System.out.println("For more information, please refer to our user guide at:" +
-                "https://ay2324s1-cs2113t-w11-1.github.io/tp/UserGuide.html");
+        System.out.println("[summary] shows a summary of all resources and the next 3 events. " +
+                "(e.g. summary)");
+        System.out.println("[exit] displays a farewell message and exits the program. " +
+                "(e.g. exit)" + System.lineSeparator());
+        System.out.println("For more information, please refer to our user guide at: https://bit.ly/SyslibUserGuide");
         showLine();
     }
 
